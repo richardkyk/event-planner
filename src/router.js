0c323e8745e1dc222b8a055b4c4cc4a37b52d2f6
Richard Khaw <28803078+Richardkyk@users.noreply.github.com>
2019-07-14 00:19:56 +1000

    Added icons for the guests

--- src/router.js
+++ src/router.js
@@ -1,25 +1,25 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Dashboard from "./views/Dashboard.vue";
+import Guests from "./views/Guests.vue";
+import Tables from "./views/Tables.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/",
+      name: "dashboard",
+      component: Dashboard
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+      path: "/guests",
+      name: "guests",
+      component: Guests
+    },
+    { path: "/tables", name: "tables", component: Tables }
   ]
-})
+});
